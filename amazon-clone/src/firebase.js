0d3a0da31dbc283777b4680565b118b826456c3c
ujjwalsotra <69038984+ujjwalsotra@users.noreply.github.com>
2022-05-23 23:09:23 +0530

    installreact app  from terminal, add firebasecode.

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBp3VjqQZRVfbt7xpEmOfruwQvBgk1afYo",
+    authDomain: "student-hub-9bbd8.firebaseapp.com",
+    projectId: "student-hub-9bbd8",
+    storageBucket: "student-hub-9bbd8.appspot.com",
+    messagingSenderId: "268329397516",
+    appId: "1:268329397516:web:796b2978f542f5a6a76d29",
+    measurementId: "G-FHQ1VTXX6S"
+  };
