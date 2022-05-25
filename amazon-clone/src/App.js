38c8741a1ba23bdee7bc28130b249dbccfba42bb
ujjwalsotra <69038984+ujjwalsotra@users.noreply.github.com>
2022-05-26 01:13:27 +0530

    Build and styled product component.

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Product from './Product';
 
 function App() {
   return (
--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Home.css";
+import Product from './Product';
 function Home() {
   return (
     <div className='home'>
@@ -9,16 +10,21 @@ function Home() {
         src='https://images-eu.ssl-images-amazon.com/images/G/31/img22/Fashion/Gateway/BAU/Clearance/PC-1500X600._CB648065009_.jpg'
          alt='Image telling amazon latest offers'/>
          <div className='home__row'>
+             <Product title='The lean startup' price={29.99} image={'https://images-na.ssl-images-amazon.com/images/I/51CTIr1bJxL._SX325_BO1,204,203,200_.jpg'} rating={4}/>
+             <Product/>
+             {/*product component*/}
+         </div>
+         <div className='home__row'>
+             {/*product component*/}
+             <Product/>
+             <Product/>
+             <Product/>
              {/*product component*/}
              {/*product component*/}
          </div>
          <div className='home__row'>
              {/*product component*/}
-             {/*product component*/}
-             {/*product component*/}
-         </div>
-         <div className='home__row'>
-             {/*product component*/}
+             <Product/>
          </div>
         </div>
     </div>
